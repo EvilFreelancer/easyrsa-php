eb1c2517272cf64ccfbe34b44743454489c1906b
Paul Rock <paul@drteam.rocks>
2020-07-08 01:33:10 +0300

    Deprecated methods removed from ConfigInterface

--- src/Interfaces/ConfigInterface.php
+++ src/Interfaces/ConfigInterface.php
@@ -9,70 +9,22 @@ interface ConfigInterface
     /**
      * Set configuration parameter
      *
-     * @param string      $name                Name of parameters
-     * @param string|null $value               Vale of parameter
-     * @param bool        $resolveAbsolutePath If need convert relative path to absolute
+     * @param string           $name                Name of parameters
+     * @param string|bool|null $value               Vale of parameter
+     * @param bool             $resolveAbsolutePath If need convert relative path to absolute
      *
      * @return \EasyRSA\Interfaces\ConfigInterface
      */
-    public function set(string $name, string $value = null, bool $resolveAbsolutePath = true): ConfigInterface;
+    public function set(string $name, $value = null, bool $resolveAbsolutePath = true): ConfigInterface;
 
     /**
      * Get parameter by name
      *
      * @param string $name
      *
-     * @return string
+     * @return string|bool|null
      */
-    public function get(string $name): string;
-
-    /**
-     * Get path with certificates.
-     *
-     * @return string
-     */
-    public function getCerts(): string;
-
-    /**
-     * Set full path to folder with certificates.
-     *
-     * @param string $path Path to folder which contains certificates
-     *
-     * @return \EasyRSA\Interfaces\ConfigInterface
-     */
-    public function setCerts(string $path): ConfigInterface;
-
-    /**
-     * Get full path to folder with scripts.
-     *
-     * @return string
-     */
-    public function getScripts(): string;
-
-    /**
-     * Set path with easy-rsa scripts.
-     *
-     * @param string $path Path to folder with EasyRSA scripts
-     *
-     * @return \EasyRSA\Interfaces\ConfigInterface
-     */
-    public function setScripts(string $path): ConfigInterface;
-
-    /**
-     * Get archive file with full path.
-     *
-     * @return string
-     */
-    public function getArchive(): string;
-
-    /**
-     * Set easy-rsa archive file with full path.
-     *
-     * @param string $path Path to file with archive
-     *
-     * @return \EasyRSA\Interfaces\ConfigInterface
-     */
-    public function setArchive(string $path): ConfigInterface;
+    public function get(string $name);
 
     /**
      * Remove '.' and '..' path parts and make path absolute without
